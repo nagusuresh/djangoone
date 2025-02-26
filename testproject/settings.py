1ea75a531d797d3fcc3bd142be47696bb1ba836a
nagusuresh <37930112+nagusuresh@users.noreply.github.com>
2025-02-26 21:34:31 +0530

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/ubuntu/djangoone/staticfiles/'
 
+STATICFILES_DIRS = [
+    # Remove 'static' if it conflicts with STATIC_ROOT
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
