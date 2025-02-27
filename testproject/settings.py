2eabb99fcc832e6f56f71814eb3803744d2fb119
nagusuresh <37930112+nagusuresh@users.noreply.github.com>
2025-02-27 08:30:27 +0530

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,11 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ubuntu/djangoone/staticfiles/'
-
-STATICFILES_DIRS = [
-    # Remove 'static' if it conflicts with STATIC_ROOT
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Collects static files here
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
