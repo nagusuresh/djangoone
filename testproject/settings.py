8d50fc2873351afc8bbf6eadaad7751a843c6b53
nagusuresh <37930112+nagusuresh@users.noreply.github.com>
2025-02-27 16:13:27 +0530

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'testproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'testproject',
+        'PASSWORD': 'mypassword',
+        'HOST': '13.61.152.162',
+        'PORT': '5432',
     }
 }
 
